0a202ed3f2b9e78adbfbc0ad4116c2e93ceb9552
Adugbo Victory <adugbovictory@gmail.com>
2023-08-29 10:44:17 +0100

    working on onboarding bug fix

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider,UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <ClerkProvider>
+      <div>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </ClerkProvider>
+  )
+}
