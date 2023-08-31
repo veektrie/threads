328ecab5dfec32d4a3f696002e7f2bfc95e45626
Adugbo Victory <adugbovictory@gmail.com>
2023-08-31 19:03:56 +0100

    components created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,44 @@
-import { ClerkProvider } from '@clerk/nextjs'
- 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
-}
- 
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+// import LeftSidebar from "@/components/shared/LeftSidebar";
+// import Bottombar from "@/components/shared/Bottombar";
+// import RightSidebar from "@/components/shared/RightSidebar";
+// import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
+    <ClerkProvider
+      appearance={{
+        // baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <main className='flex flex-row'>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+          </main>
+
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
