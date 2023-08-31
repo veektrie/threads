3c73449a71ff440a492fd52710f583e0e6077b3a
Adugbo Victory <adugbovictory@gmail.com>
2023-08-31 19:25:49 +0100

    assets added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
-// import LeftSidebar from "@/components/shared/LeftSidebar";
-// import Bottombar from "@/components/shared/Bottombar";
-// import RightSidebar from "@/components/shared/RightSidebar";
-// import Topbar from "@/components/shared/Topbar";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,13 +31,16 @@ export default function RootLayout({
     >
       <html lang='en'>
         <body className={inter.className}>
+          <Topbar/>
           <main className='flex flex-row'>
+            <LeftSidebar/>
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
             {/* @ts-ignore */}
+            <RightSidebar/>
           </main>
-
+            <Bottombar />
         </body>
       </html>
     </ClerkProvider>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <h1 className="text-white ">Home</h1>
     </div>
   )
 }
