a44b4b1ba5ee25cce70dee9a663a223d0ac7e89b
Adugbo Victory <adugbovictory@gmail.com>
2023-09-03 11:14:08 +0100

    bug fixed

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,8 +3,8 @@
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 
-// import Community from "../models/community.model";
-// import Thread from "../models/thread.model";
+import Community from "../models/commuity.model";
+import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
