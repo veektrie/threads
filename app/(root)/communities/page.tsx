30354891931b630fcd8ef082259778edb0640723
Adugbo Victory <adugbovictory@gmail.com>
2023-09-05 16:44:13 +0100

    bug fixed

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -5,7 +5,7 @@ import Searchbar from "@/components/shared/Searchbar";
 import Pagination from "@/components/shared/Pagination";
 import CommunityCard from "@/components/cards/CommunityCard";
 
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.action";
 import { fetchCommunities } from "@/lib/actions/community.actions";
 
 async function Page({
