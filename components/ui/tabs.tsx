6732e874c5dde5aff813caed44eb361e17fcd041
Adugbo Victory <adugbovictory@gmail.com>
2023-09-05 16:31:01 +0100

    final commit before commit

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import * as React from "react";
-// import * as TabsPrimitive from "@radix-ui/react-tabs";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
 import { cn } from "@/lib/validations/utils";
 
-// const Tabs = TabsPrimitive.Root;
+const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
